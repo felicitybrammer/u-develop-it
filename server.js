c5f6dda74386061110670f274294e58b753e4cc0
Felicity Brammer <felicitybrammer@gmail.com>
2021-11-02 15:10:00 -0700

    build database calls

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'baciela123',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+    );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //Default response for any other request (Not Found)
